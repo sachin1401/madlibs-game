12879499dffe4c52920052420ba1a282d14c6423
Sachin Patel <85048630+sachin1401@users.noreply.github.com>
2022-03-03 23:34:48 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+
+
+def fun_line():
+    # this line i used in madlibs game
+    # rose are red 
+    # voilet are Blue
+    # i love you
+    color=input("enter a color: ")
+    pural_noun=input("enter a pural noun: ")
+    celebrity=input("enter a celebrity: ")
+
+    print("rose are "+color+"," +pural_noun + " are blue, i love "+ celebrity +".")
+
+
+fun_line()
